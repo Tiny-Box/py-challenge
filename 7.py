3b72b00158628cc4bc78a1908ac9640a7127c4ec
airfan13 <2820655657@qq.com>
2013-10-19 01:49:45 +0800

    the seventh question's solution

--- 7.py
+++ 7.py
@@ -3,4 +3,8 @@ import urllib
 
 urllib.urlretrieve('http://www.pythonchallenge.com/pc/def/banner.p', 'banner.p')
 reader = pickle.load(open('banner.p', 'rb'))
-print reader
+for i in reader:
+	line = ""
+	for j in i:
+		line += j[0]*j[1]
+	print line
